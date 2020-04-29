44465a5f3c697ad22a15dbeb233680c669247d6a
buyunhan <1083031051@qq.com>
2020-04-29 13:34:54 +0800

    第二章

--- 第二章/Python序列类型和列表推导.py
+++ 第二章/Python序列类型和列表推导.py
@@ -3,7 +3,6 @@
 #    list、tuple、collections.deque     可存放不同类型的数据
 #扁平序列(值类型)：
 #    str、byte、bytearray、memoryview和array.array
-
 #
 #列类型还可以按照能否被修改来划分：
 #变序列：
@@ -29,4 +28,14 @@ print(codes)
 
 '''
 注意：通常只用列表推导创建新的列表
+Python3之后的版本使用列表推导之后原数组的列表也被保留了，不会再发生变量泄露
 '''
+
+
+#实际上python内置的filter和map也能实现列表推导的效果：即对可迭代类型中的元素过滤或加工，但是其可读性大打折扣
+symbols = '@#$%^&*('
+codes = [ord(symbol)for symbol in symbols]
+print(codes)
+#############
+codes = list(filter(lambda c: c > 27, map(ord, symbols)))
+print(codes)
